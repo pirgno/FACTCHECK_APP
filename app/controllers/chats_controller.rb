e3fd752a823e4ccadbfe92057febd302b5ba76db
Pierre Gayno <pierre.gayno@gmail.com>
2026-05-28 11:50:21 +0200

    commit before pull

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -5,9 +5,17 @@ class ChatsController < ApplicationController
     @chat.save
     redirect_to chat_path(@chat)
   end
-  
+
   def show
     @chat    = current_user.chats.find(params[:id])
     @message = Message.new
   end
+end
+
+def create
+  @chat = Chat.new(title: "Untitled")
+  response = RubyLLM.chat.with_instructions(instructions).ask(@message.content)
+  Message.create(role: "assistant", content: response.content, chat: @chat)
+  @chat.generate_title_from_first_message
+  @chat.user = current_user
 end
