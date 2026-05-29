8653c45376d4a8577e3514661a2d382ae97bf489
mathieu tempere <mathieutempere1@gmail.com>
2026-05-29 14:51:36 +0200

    delete done

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,6 +1,6 @@
 class NewsController < ApplicationController
   def index
     @news = News.where(trending: true).limit(4)
-    @saved_messages = current_user.saved_chat_messages
+    @saved_messages = current_user.saved_messages
   end
 end
